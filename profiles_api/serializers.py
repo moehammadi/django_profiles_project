20495526af42146c0a65e331fe2a7add1e67f95a
Mohammad Hammadi <hammadimohammad@gmail.com>
2020-12-17 17:27:51 +0200

    Added Profiles viewset, Permissions

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -1,6 +1,35 @@
 from rest_framework import serializers
+from .models import UserProfile
 
 
 class HelloSerializer(serializers.Serializer):
     """Serializes a name field for testing out APIView"""
     name = serializers.CharField(max_length=10)
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    """Serializes a user profile object"""
+
+    class Meta:
+        model = UserProfile
+        fields = ('id', 'email', 'name', 'password')
+
+        # Set password to write only, so it cannot be retrieved. Set input type to password
+        extra_kwargs = {
+            'password': {
+                'write_only': True,
+                'style': {
+                    'input_type': 'password'
+                }
+            }
+        }
+
+    def create(self, validated_data):
+        """Create and return a new user. Override functionality to use create_user function """
+        user = UserProfile.objects.create_user(
+            email=validated_data['email'],
+            name=validated_data['name'],
+            password=validated_data['password'],
+        )
+
+        return user
--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -2,11 +2,12 @@ from django.urls import path, include
 
 from rest_framework.routers import DefaultRouter
 
-from .views import HelloAPIView, HelloViewSet
+from .views import HelloAPIView, HelloViewSet, UserProfileViewSet
 
 router = DefaultRouter()
 router.register('hello-viewset', HelloViewSet, basename='hello-viewset')
-
+# Since UserProfileViewSet has a query set, don't assign a basename.
+router.register('profile', UserProfileViewSet)
 
 urlpatterns = [
     path('hello-view/', HelloAPIView.as_view()),
